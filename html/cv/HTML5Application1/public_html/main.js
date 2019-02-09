3cd43ab4e8d57013df7ffcca6ffb0ab045cffc30
zbynioo85 <zbynioo85@gmail.com>
2019-02-09 22:17:16 +0100

    no message

--- html/cv/HTML5Application1/public_html/main.js
+++ html/cv/HTML5Application1/public_html/main.js
@@ -1,8 +1,15 @@
-const burger = document.querySelector(".burger");
-const activeElement = document.querySelectorAll(".active");
-
-burger.addEventListener("click", function () {
+const burger = document.querySelector('.burger');
+const activeElement = document.querySelectorAll('.active');
+const navLi = document.querySelectorAll('nav li');
+burger.addEventListener('click', function () {
     for (let i = 0; i < activeElement.length; i++) {
         activeElement[i].classList.toggle("show");
     }
 });
+navLi.forEach(function (items) {
+    items.addEventListener('click', function () {
+        for (let i = 0; i < activeElement.length; i++) {
+            activeElement[i].classList.toggle("show");
+        }
+    })
+})
